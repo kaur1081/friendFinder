9b1b07809b1efa7f0d8db1bf0081420978647df2
kaur1081 <karishmawalia@gmail.com>
2019-11-24 15:01:09 -0800

    display result is working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,9 @@ var friends = require("../data/friends.js");
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
+        console.log(req);
         res.json(friends);
+
     });
     app.post("/api/friends", function (req, res) {
         var bestMatch = {
@@ -10,23 +12,25 @@ module.exports = function (app) {
             photo: "",
             friendDifference: 1000
         };
-        console.log(req.body);
+        // console.log(req.body);
 
 
         var userData = req.body;
         var userScores = userData.scores;
-        console.log(userScores);
+        console.log(userData.scores);
 
         var totalDifference = 0;
 
         for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i]);
+            // console.log(friends[i]);
             totalDifference = 0;
 
             // loop through each friend
             for (var j = 0; j < friends[i].scores[j]; j++) {
-
+// console.log("firneds:", friends[i], "friends scores:", friends[i].scores[j]);
+                console.log(userScores[j]);
                 totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
                 if (totalDifference <= bestMatch.friendDifference) {
 
                     bestMatch.name = friends[i].name;
@@ -36,8 +40,9 @@ module.exports = function (app) {
 
             }
         }
-friends.push(userData);
-res.json(bestMatch);
+        console.log(bestMatch);
+    friends.push(userData);
+    res.json(bestMatch);
 
 
     });
